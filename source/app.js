75a049e1ff13c9b557c673849a786c906e66c203
Jarek Pelczynski <jpelczynski@gmail.com>
2015-11-27 16:06:51 +0100

    Fix PTF after PT update

--- source/app.js
+++ source/app.js
@@ -1,25 +1,22 @@
-jQuery(function (){
-  console.log('Rock & load!')
+console.log('Rock & load!')
 
-  $(document.body).on('click', function (e) {
-    var target = e.target;
-    if ( $(target).hasClass('expander') ) {
-      appendButton( $(target).parents('.story.item.model').data('id') );
-    }
-  });
+var initButton = function () {
+  var $stories, storyID, storyName, storyFormat;
 
-  var appendButton = function (target) {
-    var $stories, storyID, storyName, storyFormat;
+  if ( $(this).hasClass('expander') ) {
+    storyID = $(this).parents('.story.item.model').data('id');
+  }
 
-    $stories = $('.story.item');
-
-    if (target) {
-      $stories = $('.story_' + target).last();
+  setTimeout(function () {
+    if (storyID) {
+      $stories = $('.story_' + storyID).last();
+    } else {
+      $stories = $('.story.item');
     }
 
     $.each($stories, function () {
       $story      = $(this);
-      storyID     = target || $story.attr('class').match(/\d+/);
+      storyID     = $story.attr('class').match(/\d+/);
       storyName   = $story.find('.story_name').text() || $story.find('textarea.name').text();
       storyFormat = '[PT ' + storyID + '] ' + storyName;
 
@@ -31,16 +28,27 @@ jQuery(function (){
         e.preventDefault();
       });
 
-      $wrap = $story.find('.edit').find('.controls').find('.actions');
+      $wrap = $story.find('.wrapper').find('.controls').find('.actions');
 
       $wrap.prepend( $button );
       $wrap.find('button.link.left_endcap').toggleClass('left_endcap capped')
       $wrap.css({ 'width' : '315px' });
     });
+  }, 100)
 
-  };
+};
 
-  $(document.body).on('click', '.expander', appendButton)
+$(document.body).on('click.ptf', '.expander', initButton);
 
-  setTimeout(appendButton, 1000);
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var list = document.querySelector('div');
+
+var observer = new MutationObserver(function(mutations) {
+  initButton()
+});
+
+observer.observe(list, {
+  attributes: true,
+  childList: true,
+  characterData: true
 });
